ebcf3680bfe97b880615069791853c0280ec05d5
Jeremy <hajek@iit.edu>
2025-08-06 23:09:07 -0500

    add additional Flask templates and css

--- build/packer/scripts/proxmox/three-tier/frontend/post_install_prxmx_ubuntu_create_service_account_for_flask_app.sh
+++ build/packer/scripts/proxmox/three-tier/frontend/post_install_prxmx_ubuntu_create_service_account_for_flask_app.sh
@@ -11,6 +11,8 @@ sudo adduser --system --group flaskuser
 
 sudo mv /home/vagrant/CyberRange/code/python-flask/app.py /home/flaskuser/app.py
 sudo mv /home/vagrant/CyberRange/code/python-flask/.env /home/flaskuser/.env
+sudo mv /home/vagrant/CyberRange/code/python-flask/static/ /home/flaskuser/static/
+sudo mv /home/vagrant/CyberRange/code/python-flask/templates/ /home/flaskuser/templates/
 # How to use an ENV variable in a sed command
 # https://askubuntu.com/questions/76808/how-do-i-use-variables-in-a-sed-command
 sed -i "s/REPLACE/$APPVAULT_TOKEN/g" /home/flaskuser/.env
