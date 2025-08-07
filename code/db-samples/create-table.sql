f10788d4a9257409306405856de957a9c4538528
Jeremy <hajek@iit.edu>
2025-08-07 00:04:42 -0500

    add timestamp field to create

--- code/db-samples/create-table.sql
+++ code/db-samples/create-table.sql
@@ -7,13 +7,15 @@ CREATE TABLE users
 ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 uuid VARCHAR(128),
 email VARCHAR(128),
-emailid VARCHAR(128),
-gtoken VARCHAR(128)
+email_id VARCHAR(128),
+g_token VARCHAR(128),
+created_at TIMESTAMP
 );
 
 CREATE TABLE status
 (
 ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-labnumber VARCHAR(32),    
-grade FLOAT(3, 2)
+lab_number VARCHAR(32),    
+grade FLOAT(3, 2),
+created_at TIMESTAMP
 );
