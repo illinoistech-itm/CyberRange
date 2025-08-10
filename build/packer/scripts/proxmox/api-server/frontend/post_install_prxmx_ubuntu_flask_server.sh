62f71be6af69fad975e371e9fc0ff7cb53c2c3c6
Jeremy <hajek@iit.edu>
2025-08-10 16:41:30 -0500

    update remove incorrect package

--- build/packer/scripts/proxmox/api-server/frontend/post_install_prxmx_ubuntu_flask_server.sh
+++ build/packer/scripts/proxmox/api-server/frontend/post_install_prxmx_ubuntu_flask_server.sh
@@ -17,7 +17,7 @@ sudo apt install -y gunicorn python3-flask
 # Requirements for Flask app functionality
 sudo apt install -y python3-flask-socketio python3-requests python3-hvac python3-dotenv python3-proxmoxer python3-flask-sqlalchemy python3-metaconfig
 # Install for local logging for the moment -- eventually move to ELK stack
-sudo apt install -y sqlite3 sqlite-utils
+sudo apt install -y sqlite3
 
 # Move the service file into /etc/systemd/system which is where user created
 # service files are placed by convention
