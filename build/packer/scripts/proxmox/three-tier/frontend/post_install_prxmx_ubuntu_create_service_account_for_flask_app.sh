6d91efa417a5d15fc2afc8b6efc92ddf5cf8018d
Jeremy <hajek@iit.edu>
2025-08-10 19:23:27 -0500

    update copy command and chown command

--- build/packer/scripts/proxmox/three-tier/frontend/post_install_prxmx_ubuntu_create_service_account_for_flask_app.sh
+++ build/packer/scripts/proxmox/three-tier/frontend/post_install_prxmx_ubuntu_create_service_account_for_flask_app.sh
@@ -11,6 +11,7 @@ sudo adduser --system --group flaskuser
 
 sudo mv /home/vagrant/CyberRange/code/python-flask/app.py /home/flaskuser/app.py
 sudo mv /home/vagrant/CyberRange/code/python-flask/.env /home/flaskuser/.env
+sudo mv /home/vagrant/id_ed25519_paramiko_connect_key_from_flask_app_to_lab_edge_node /home/flaskuser/id_ed25519_paramiko_connect_key_from_flask_app_to_lab_edge_node
 sudo mv /home/vagrant/CyberRange/code/python-flask/static/ /home/flaskuser/static/
 sudo mv /home/vagrant/CyberRange/code/python-flask/templates/ /home/flaskuser/templates/
 # Move all the lab question and answer files over
@@ -19,4 +20,4 @@ sudo mv /home/vagrant/CyberRange/code/python-flask/*.toml /home/flaskuser/
 # How to use an ENV variable in a sed command
 # https://askubuntu.com/questions/76808/how-do-i-use-variables-in-a-sed-command
 sed -i "s/REPLACE/$APPVAULT_TOKEN/g" /home/flaskuser/.env
-sudo chown -R flaskuser:flaskuser /home/flaskuser/app.py
+sudo chown -R flaskuser:flaskuser /home/flaskuser/*
