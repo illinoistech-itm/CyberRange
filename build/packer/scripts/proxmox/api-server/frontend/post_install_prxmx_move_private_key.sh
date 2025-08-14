694406d6becfd76bcc3356d4a51523eaf2a819de
Jeremy <hajek@iit.edu>
2025-08-13 22:17:41 -0500

    update mv key for flaskuser to connect to the buildserver

---
+++ build/packer/scripts/proxmox/api-server/frontend/post_install_prxmx_move_private_key.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Moving private key used to connect to the build server
+
+sudo mv /home/vagrant/id_ed25519_flask_api_to_buildserver_connect_key /home/flaskuser/id_ed25519_flask_api_to_buildserver_connect_key
