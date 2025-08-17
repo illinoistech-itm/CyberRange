4a4264aaea8c21accdbe84571a5217ff677776ee
Jeremy <hajek@iit.edu>
2025-08-16 19:00:29 -0500

    update lab_number to be a varchar

--- code/db-samples/create-table.sql
+++ code/db-samples/create-table.sql
@@ -19,7 +19,7 @@ id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
 -- In MySQL, there isn't a native BOOLEAN data type. Instead, BOOLEAN is 
 -- treated as a synonym for TINYINT(1), where 0 represents FALSE and 1
 -- represents TRUE.
-lab_number INT(10),
+lab_number VARCHAR(64),
 grade INT(3) DEFAULT 0,
 lab_complete TINYINT(1) DEFAULT 0,
 last_attempt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
