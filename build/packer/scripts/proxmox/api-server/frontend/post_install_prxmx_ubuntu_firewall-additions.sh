1c8b1a275995c34bd30b4b404f4af14472282374
Jeremy <hajek@iit.edu>
2025-09-08 00:13:26 -0500

    update adding port 3000 opening in public zone

--- build/packer/scripts/proxmox/api-server/frontend/post_install_prxmx_ubuntu_firewall-additions.sh
+++ build/packer/scripts/proxmox/api-server/frontend/post_install_prxmx_ubuntu_firewall-additions.sh
@@ -9,6 +9,7 @@ set -v
 ##############################################################################################
 # Open port 3000 on the internal meta-network port 3000 for our Flask App
 sudo firewall-cmd --zone=meta-network --add-port=3000/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=3000/tcp --permanent
 
 # Restart the firewall to reload the rules
 sudo firewall-cmd --reload
