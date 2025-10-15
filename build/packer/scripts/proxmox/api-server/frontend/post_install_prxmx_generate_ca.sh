7f9229fce7959921460b93329fce84d7f6fee629
Ana Rosiles <122835889+aros14@users.noreply.github.com>
2025-10-15 16:55:17 -0500

    adding fingerprint

--- build/packer/scripts/proxmox/api-server/frontend/post_install_prxmx_generate_ca.sh
+++ build/packer/scripts/proxmox/api-server/frontend/post_install_prxmx_generate_ca.sh
@@ -4,7 +4,7 @@
 
 # https://4sysops.com/archives/install-and-use-the-step-ca-certificate-authority-client/
 # https://4sysops.com/archives/step-ca-running-your-own-certificate-authority-with-acme-support/
-sudo step ca bootstrap --ca-url https://system36.rice.iit.edu --fingerprint 
+sudo step ca bootstrap --ca-url https://system36.rice.iit.edu --fingerprint f230790d390351b320c22adace7ad894eef3a94e79c17385f07956e8264267fc
 sudo step certificate install --all ~/.step/certs/root_ca.crt
 sudo TOKEN=$(step ca token system36.rice.iit.edu --provisioner-password 'aaS9UoZFnEfpECJI3dEgJtzHM6Q6W1xK') # find how to pass through the password
 sudo step ca certificate --token $TOKEN system36.rice.iit.edu CAcr.crt CAcr.key
