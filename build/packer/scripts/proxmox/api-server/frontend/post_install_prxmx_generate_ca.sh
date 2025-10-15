8b311a054da3c958f6958e6f859eac824609fd32
Ana Rosiles <122835889+aros14@users.noreply.github.com>
2025-10-15 16:03:34 -0500

    creating copy of gen ss ca to make signed ca

---
+++ build/packer/scripts/proxmox/api-server/frontend/post_install_prxmx_generate_ca.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Generate a signed certificate 
+
+# https://stackoverflow.com/questions/10175812/how-to-create-a-self-signed-certificate-with-openssl
+# https://ethitter.com/2016/05/generating-a-csr-with-san-at-the-command-line/
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /home/flaskuser/selfsigned.key -out /home/flaskuser/selfsigned.crt -subj "/C=US/ST=IL/L=Chicago/O=IIT/OU=rice/CN=iit.edu"
+sudo chown flaskuser:flaskuser /home/flaskuser/selfsigned.key
+sudo chown flaskuser:flaskuser /home/flaskuser/selfsigned.crt
