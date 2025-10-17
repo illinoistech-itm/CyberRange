ee6ce4291523e9ccd4310e17c30bc83e52deb77f
Ana Rosiles <122835889+aros14@users.noreply.github.com>
2025-10-17 11:35:30 -0500

    adding comment for password and fingerprint retreival

--- build/packer/scripts/proxmox/api-server/frontend/post_install_prxmx_generate_ca.sh
+++ build/packer/scripts/proxmox/api-server/frontend/post_install_prxmx_generate_ca.sh
@@ -4,6 +4,8 @@
 
 # https://4sysops.com/archives/install-and-use-the-step-ca-certificate-authority-client/
 # https://4sysops.com/archives/step-ca-running-your-own-certificate-authority-with-acme-support/
+
+# have to find a way to dynamically get the password and fingerprint from the CA server
 sudo step ca bootstrap --ca-url https://system36.rice.iit.edu --fingerprint f230790d390351b320c22adace7ad894eef3a94e79c17385f07956e8264267fc
 sudo step certificate install --all ~/.step/certs/root_ca.crt
 sudo TOKEN=$(step ca token system36.rice.iit.edu --provisioner-password 'aaS9UoZFnEfpECJI3dEgJtzHM6Q6W1xK') # find how to pass through the password
