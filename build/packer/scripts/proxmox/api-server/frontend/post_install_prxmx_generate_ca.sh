12f3e48713f74a02f9b770a790b79bbb7f2a617f
Ana Rosiles <122835889+aros14@users.noreply.github.com>
2025-10-17 18:29:59 -0500

    fixing install path

--- build/packer/scripts/proxmox/api-server/frontend/post_install_prxmx_generate_ca.sh
+++ build/packer/scripts/proxmox/api-server/frontend/post_install_prxmx_generate_ca.sh
@@ -11,7 +11,7 @@ sudo dpkg -i step-cli_amd64.deb
 
 # have to find a way to dynamically get the password and fingerprint from the CA server
 sudo step ca bootstrap --ca-url https://system36.rice.iit.edu --fingerprint 095ecc2996e06e9c84d4d1ff211fefb545e4097a3cde68249ecb04cfe231a1b7
-sudo step certificate install --all ~/.step/certs/root_ca.crt
+sudo step certificate install --all /root/.step/certs/root_ca.crt
 echo "158234246165263303871269841982826793299" > password.txt
 TOKEN=$(step ca token system36.rice.iit.edu --provisioner-password-file=password.txt)
 sudo step ca certificate --token $TOKEN system36.rice.iit.edu CAcr.crt CAcr.key
