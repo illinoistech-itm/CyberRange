7dc95fd3f0df5d63e4fa201ebd50014fe8bcb7c3
Cedric Nartey <cnartey1@hawk.illinoistech.edu>
2025-11-05 17:03:46 -0600

    second comment for ca path

--- build/packer/scripts/proxmox/api-server/frontend/post_install_prxmx_generate_ca.sh
+++ build/packer/scripts/proxmox/api-server/frontend/post_install_prxmx_generate_ca.sh
@@ -14,6 +14,7 @@ sudo dpkg -i step-cli_amd64.deb
 #sudo step ca bootstrap --ca-url https://system36.rice.iit.edu --fingerprint 81834205d8387947c53adb24a4ca6f6eca1c54e7602f30ebfd584cae62cc9c4c
 sudo step ca bootstrap --ca-url https://system36.rice.iit.edu --fingerprint $FINGERPRINT
 sudo step certificate install --all /etc/step-ca/certs/root_ca.crt
+sudo step certificate install --all /root/.step/certs/root_ca.crt
 echo "158234246165263303871269841982826793299" > password.txt
 
 # explaining the flags https://smallstep.com/docs/step-cli/reference/ca/token/
