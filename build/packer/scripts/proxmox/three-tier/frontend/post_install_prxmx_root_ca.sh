f3f952149b99888822ef14289b82160eac2f521f
Cedric Nartey <cnartey1@hawk.illinoistech.edu>
2025-11-12 16:10:52 -0600

    added comment

--- build/packer/scripts/proxmox/three-tier/frontend/post_install_prxmx_root_ca.sh
+++ build/packer/scripts/proxmox/three-tier/frontend/post_install_prxmx_root_ca.sh
@@ -8,5 +8,5 @@ wget https://dl.smallstep.com/cli/docs-cli-install/latest/step-cli_amd64.deb
 sudo dpkg -i step-cli_amd64.deb
 
 # have to find a way to dynamically get the password and fingerprint from the CA server
-sudo step ca bootstrap --ca-url https://system36.rice.iit.edu --fingerprint 81834205d8387947c53adb24a4ca6f6eca1c54e7602f30ebfd584cae62cc9c4c
+sudo step ca bootstrap --ca-url https://system36.rice.iit.edu --fingerprint 81834205d8387947c53adb24a4ca6f6eca1c54e7602f30ebfd584cae62cc9c4c #Potentially adjust to use variable FINGERPRINT instead 
 sudo step certificate install --all /root/.step/certs/root_ca.crt
