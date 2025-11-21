e2a970145dfa0891af57a10f9c17a35ac9cffc01
Jeremy <hajek@iit.edu>
2025-11-21 08:14:44 -0600

    update adding ssh access to meta-network

--- build/packer/scripts/proxmox/three-tier/logserver/post_install_prxmx_logserver-firewall-open-ports.sh
+++ build/packer/scripts/proxmox/three-tier/logserver/post_install_prxmx_logserver-firewall-open-ports.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo firewall-cmd --zone=meta-network --add-port=3001/tcp --permanent
+sudo firewall-cmd --zone=meta-network --add-service=ssh --permanent
 
 sudo firewall-cmd --reload
