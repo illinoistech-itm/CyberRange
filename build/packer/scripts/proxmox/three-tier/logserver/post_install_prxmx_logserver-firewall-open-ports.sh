076f39d06bcfa5deefef83d053c9125b25437596
Jeremy <hajek@iit.edu>
2025-11-21 08:24:30 -0600

    Update port incorrectly had 3001 should be 3100

--- build/packer/scripts/proxmox/three-tier/logserver/post_install_prxmx_logserver-firewall-open-ports.sh
+++ build/packer/scripts/proxmox/three-tier/logserver/post_install_prxmx_logserver-firewall-open-ports.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# Configure 3100 port open to receive systemd logs
 
-sudo firewall-cmd --zone=meta-network --add-port=3001/tcp --permanent
+sudo firewall-cmd --zone=meta-network --add-port=3100/tcp --permanent
 sudo firewall-cmd --zone=meta-network --add-service=ssh --permanent
 
 sudo firewall-cmd --reload
