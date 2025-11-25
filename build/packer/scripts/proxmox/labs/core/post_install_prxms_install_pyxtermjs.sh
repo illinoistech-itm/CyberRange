7936e45a2ea04eaa9ba5b53a6d9fda65e4151780
Jeremy <hajek@iit.edu>
2025-11-24 23:52:42 -0600

    remove duplicated evenlet

--- build/packer/scripts/proxmox/labs/core/post_install_prxms_install_pyxtermjs.sh
+++ build/packer/scripts/proxmox/labs/core/post_install_prxms_install_pyxtermjs.sh
@@ -8,4 +8,3 @@ sudo apt install pipx -y
 # sudo -u vagrant is to ensure that the packages are installed as vagrant not as root
 sudo -u vagrant pipx install pyxtermjs
 sudo -u vagrant pipx ensurepath 
-sudo apt install python3-eventlet -y
