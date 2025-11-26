78f6e4591129941b46940898b7352675ebfe8d4a
Jeremy <hajek@iit.edu>
2025-11-26 14:15:23 -0600

    hardcode fingerpritn for the moment

--- build/packer/scripts/proxmox/labs/core/post_install_prxmx_generate_ca.sh
+++ build/packer/scripts/proxmox/labs/core/post_install_prxmx_generate_ca.sh
@@ -11,8 +11,8 @@ sudo dpkg -i step-cli_amd64.deb
 
 # have to find a way to dynamically get the password and fingerprint from the CA server
 #sudo step ca bootstrap --ca-url https://system36.rice.iit.edu --fingerprint 095ecc2996e06e9c84d4d1ff211fefb545e4097a3cde68249ecb04cfe231a1b7 
-#sudo step ca bootstrap --ca-url https://system36.rice.iit.edu --fingerprint 81834205d8387947c53adb24a4ca6f6eca1c54e7602f30ebfd584cae62cc9c4c
-sudo step ca bootstrap --ca-url https://system36.rice.iit.edu --fingerprint $FINGERPRINT
+sudo step ca bootstrap --ca-url https://system36.rice.iit.edu --fingerprint 81834205d8387947c53adb24a4ca6f6eca1c54e7602f30ebfd584cae62cc9c4c
+#sudo step ca bootstrap --ca-url https://system36.rice.iit.edu --fingerprint $FINGERPRINT
 #sudo step certificate install --all /etc/step-ca/certs/root_ca.crt
 sudo step certificate install --all /root/.step/certs/root_ca.crt
 #echo "158234246165263303871269841982826793299" > password.txt <--commented out to test alternative, add back in if fails
