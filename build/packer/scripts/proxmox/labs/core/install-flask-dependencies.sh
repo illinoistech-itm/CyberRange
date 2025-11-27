e84a4288d7105436d55f0fdc08d762fec0cd027e
Jeremy <hajek@iit.edu>
2025-11-27 15:48:56 -0600

    adding eventlet

--- build/packer/scripts/proxmox/labs/core/install-flask-dependencies.sh
+++ build/packer/scripts/proxmox/labs/core/install-flask-dependencies.sh
@@ -5,4 +5,4 @@ sudo apt install -y python3-flask python3-flask-socketio
 # Dependencies for the wss-proxy.service -- we are not running this via pipx that is only for the 
 # pyxtermjs which we will install/inject dependencies via pipx...  python...
 
-sudo apt install -y python3-dev gunicorn python3-gevent python3-websocket
+sudo apt install -y python3-dev gunicorn python3-gevent python3-websocket python3-eventlet
