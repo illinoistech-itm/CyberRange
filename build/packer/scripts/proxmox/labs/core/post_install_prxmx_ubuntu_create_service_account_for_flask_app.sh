362ea8a7857376ce21648a9159fe5b90163f6e79
Jeremy <hajek@iit.edu>
2025-11-27 20:46:54 -0600

    Add logix to move toml files to correct location

--- build/packer/scripts/proxmox/labs/core/post_install_prxmx_ubuntu_create_service_account_for_flask_app.sh
+++ build/packer/scripts/proxmox/labs/core/post_install_prxmx_ubuntu_create_service_account_for_flask_app.sh
@@ -10,5 +10,6 @@ sudo adduser --system --group flaskuser
 # https://copilot.microsoft.com/shares/nis5fbJZkaup7K34PRKwK
 
 sudo mv /home/vagrant/CyberRange/code/lab-core/app.py /home/flaskuser/app.py
+sudo mv /home/vagrant/CyberRange/code/python-flask/labs /home/flaskuser/labs
 
 sudo chown -R flaskuser:flaskuser /home/flaskuser/app.py
