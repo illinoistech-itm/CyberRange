fa7cc7d9144249d2e37b51e338e19e6d81fbd62f
Jeremy <hajek@iit.edu>
2025-11-29 22:03:48 -0600

    Removed WSS proxy

--- build/packer/scripts/proxmox/labs/core/move-wss-proxy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo mv /home/vagrant/wss-proxy.service /etc/systemd/system/
-
-sudo systemctl enable wss-proxy.service
--- build/packer/scripts/proxmox/labs/core/post_install_prxms_install_pyxtermjs.sh
+++ build/packer/scripts/proxmox/labs/core/post_install_prxms_install_pyxtermjs.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-############################################################################################
+###############################################################################
 # Script to install all the pre-reqs needed to instantiate pyxtermjs 
-#############################################################################################
+###############################################################################
 
 sudo apt install pipx -y
-# sudo -u vagrant is to ensure that the packages are installed as vagrant not as root
-# Need to inject each package into the others virtual environment otherwise you will
-# receive a module not found error
+# sudo -u vagrant is to ensure that the packages are installed as vagrant not 
+# as root
+# Need to inject each package into the others virtual environment otherwise 
+# you will receive a: "Module not found error."
 sudo -u flaskuser pipx install pyxtermjs
 sudo -u flaskuser pipx install gunicorn
 sudo -u flaskuser pipx inject pyxtermjs gunicorn eventlet gevent
 sudo -u flaskuser pipx inject gunicorn pyxtermjs eventlet gevent
 sudo -u flaskuser pipx inject flask python-socketio python-engineio flask-socketio
 
-# Using sed to replace the app instantiation and wrap it in a CORS allow Origin for system60
+# Using sed to replace the app instantiation and wrap it in a CORS allow Origin
+# for system60
 sudo sed -i 's/socketio = SocketIO(app)/socketio = SocketIO(app, cors_allowed_origins="*")/g' /home/flaskuser/.local/pipx/venvs/pyxtermjs/lib/python3.10/site-packages/pyxtermjs/app.py
 sudo sed -i 's/socketio = SocketIO(app)/socketio = SocketIO(app, cors_allowed_origins="*")/g' /home/flaskuser/.local/pipx/venvs/gunicorn/lib/python3.10/site-packages/pyxtermjs/app.py
+
+# Comment these two lines the pyxtermjs app.py to log at INFO level not ERROR 
+# turn off if you don't need to debug. 
+# The value being changed is in this line:
+# logging.getLogger("werkzeug").setLevel(logging.ERROR) to
+# logging.getLogger("werkzeug").setLevel(logging.INFO)
+
+sudo sed -i 's/ERROR/INFO/g' /home/flaskuser/.local/pipx/venvs/gunicorn/lib/python3.10/site-packages/pyxtermjs/app.py
+sudo sed -i 's/ERROR/INFO/g' /home/flaskuser/.local/pipx/venvs/pyxtermjs/lib/python3.10/site-packages/pyxtermjs/app.py
