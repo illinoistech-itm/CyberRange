260a2212801dd8204e1ea64a789b8990381e07f2
Jeremy <hajek@iit.edu>
2025-11-30 17:59:19 -0600

    update sed work

--- build/packer/scripts/proxmox/labs/core/post_install_prxms_install_pyxtermjs.sh
+++ build/packer/scripts/proxmox/labs/core/post_install_prxms_install_pyxtermjs.sh
@@ -10,20 +10,15 @@ sudo apt install pipx -y
 # Need to inject each package into the others virtual environment otherwise 
 # you will receive a: "Module not found error."
 sudo -u flaskuser pipx install pyxtermjs
-sudo -u flaskuser pipx install gunicorn
 sudo -u flaskuser pipx inject pyxtermjs gunicorn eventlet gevent
-sudo -u flaskuser pipx inject gunicorn pyxtermjs eventlet gevent
 # When you install pyxtermjs (whether via pip, pipx, or inside a virtualenv), 
 # you don’t need to separately install Flask yourself — the package already 
 # declares Flask (and Flask‑SocketIO, python‑socketio, python‑engineio, etc.)
 # as dependencies.
 
-#sudo -u flaskuser pipx inject flask python-socketio python-engineio flask-socketio
-
 # Using sed to replace the app instantiation and wrap it in a CORS allow Origin
 # for system60
 sudo sed -i 's/socketio = SocketIO(app)/socketio = SocketIO(app, cors_allowed_origins="*")/g' /home/flaskuser/.local/pipx/venvs/pyxtermjs/lib/python3.10/site-packages/pyxtermjs/app.py
-sudo sed -i 's/socketio = SocketIO(app)/socketio = SocketIO(app, cors_allowed_origins="*")/g' /home/flaskuser/.local/pipx/venvs/gunicorn/lib/python3.10/site-packages/pyxtermjs/app.py
 
 # Comment these two lines the pyxtermjs app.py to log at INFO level not ERROR 
 # turn off if you don't need to debug. 
@@ -31,5 +26,8 @@ sudo sed -i 's/socketio = SocketIO(app)/socketio = SocketIO(app, cors_allowed_or
 # logging.getLogger("werkzeug").setLevel(logging.ERROR) to
 # logging.getLogger("werkzeug").setLevel(logging.INFO)
 
-sudo sed -i 's/ERROR/INFO/g' /home/flaskuser/.local/pipx/venvs/gunicorn/lib/python3.10/site-packages/pyxtermjs/app.py
 sudo sed -i 's/ERROR/INFO/g' /home/flaskuser/.local/pipx/venvs/pyxtermjs/lib/python3.10/site-packages/pyxtermjs/app.py
+# Replace out fo the sample code the namespace /pty not listening on that
+sudo sed -i 's/, namespace="\/pty"//g' /home/flaskuser/.local/pipx/venvs/pyxtermjs/lib/python3.10/site-packages/pyxtermjs/app.py
+# replace the data being send to the websocket instead of a dict" data["input"] it should be a string: data
+sudo sed -i 's/data\["input"\]/data/g' /home/flaskuser/.local/pipx/venvs/pyxtermjs/lib/python3.10/site-packages/pyxtermjs/app.py
