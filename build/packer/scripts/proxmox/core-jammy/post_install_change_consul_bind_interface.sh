32749d031bbef32542cbb2b88ba81ac281c1315c
Jeremy <hajek@iit.edu>
2025-11-30 15:06:57 -0600

    add sudo command

--- build/packer/scripts/proxmox/core-jammy/post_install_change_consul_bind_interface.sh
+++ build/packer/scripts/proxmox/core-jammy/post_install_change_consul_bind_interface.sh
@@ -2,8 +2,8 @@
 
 #########################################################################
 # Script to change the bind_addr in Consul to the dynmaic Go lang call to
-# Interface ens18
+# Interface ens20
 # https://www.consul.io/docs/troubleshoot/common-errors
 #########################################################################
 
-sed -i 's/#bind_addr = \"0.0.0.0\"/bind_addr = \"{{GetInterfaceIP \\\"ens20\\\"}}\"/' /etc/consul.d/consul.hcl
+sudo sed -i 's/#bind_addr = \"0.0.0.0\"/bind_addr = \"{{GetInterfaceIP \\\"ens20\\\"}}\"/' /etc/consul.d/consul.hcl
