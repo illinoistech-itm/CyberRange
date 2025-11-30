47c933b4365da9be83f3b3901190a3519d4c9f64
Jeremy <hajek@iit.edu>
2025-11-29 23:23:28 -0600

    removed uneeded chown

--- build/packer/scripts/proxmox/labs/core/post_install_prxmx_ubuntu_create_service_account_for_flask_app.sh
+++ build/packer/scripts/proxmox/labs/core/post_install_prxmx_ubuntu_create_service_account_for_flask_app.sh
@@ -12,4 +12,4 @@ sudo adduser --system --group flaskuser
 #sudo mv /home/vagrant/CyberRange/code/lab-core/app.py /home/flaskuser/app.py
 #sudo mv /home/vagrant/CyberRange/code/python-flask/labs /home/flaskuser/labs
 
-sudo chown -R flaskuser:flaskuser /home/flaskuser/app.py
+#sudo chown -R flaskuser:flaskuser /home/flaskuser/app.py
