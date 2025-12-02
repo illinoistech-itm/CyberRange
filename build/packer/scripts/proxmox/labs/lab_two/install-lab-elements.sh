00704519b5ead56db05072f9f1d79621f12ac498
Jeremy <hajek@iit.edu>
2025-12-02 16:03:26 -0600

    update to add exposed maria db

--- build/packer/scripts/proxmox/labs/lab_two/install-lab-elements.sh
+++ build/packer/scripts/proxmox/labs/lab_two/install-lab-elements.sh
@@ -3,6 +3,9 @@
 # Just a simple script to install and configure the elements of the lab
 sudo apt update
 
-sudo apt install -y mariadb
+sudo apt install -y mariadb-server
 
-sudo systemctl enable mariadb
+# Open database to listen externally
+sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+sudo systemctl enable mariadb.service
