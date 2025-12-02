243b04438ed8cbcc29f85c734a445e1f55699290
Jeremy <hajek@iit.edu>
2025-12-02 16:34:02 -0600

    update adding terraform for lab_two

---
+++ build/terraform/proxmox-jammy-ubuntu-cr-lab-templates/lab_two/main.tf
@@ -0,0 +1,252 @@
+###############################################################################################
+# This template demonstrates a Terraform plan to deploy two custom Ubuntu Focal 22.04 instances
+###############################################################################################
+resource "random_id" "id" {
+  byte_length = 8
+}
+
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/shuffle#example-usage
+resource "random_shuffle" "datadisk" {
+  input        = ["cyberrange"]
+  result_count = 1
+}
+# data.vault_generic_secret.target_node.data
+resource "random_shuffle" "nodename" {
+  input        = [data.vault_generic_secret.target_node.data["SYSTEM42"], data.vault_generic_secret.target_node.data["SYSTEM41"]]
+  result_count = 1
+}
+
+resource "random_integer" "vlan" {
+  min = 10
+  max = 4095
+}
+
+output "random_number" {
+  value = random_integer.vlan.result
+}
+
+##############################################################################
+# Connecting Vault with Secrets for Terraform
+# https://registry.terraform.io/providers/hashicorp/vault/latest/docs/data-sources/generic_secret
+# https://registry.terraform.io/providers/hashicorp/vault/latest/docs
+# https://github.com/hashicorp/terraform/issues/16457
+##############################################################################
+data "vault_generic_secret" "pm_api_url" {
+  path = "secret/URL"
+}
+
+data "vault_generic_secret" "pm_api_token_id" {
+  path = "secret/SECRETKEY"
+}
+
+data "vault_generic_secret" "pm_api_token_secret" {
+  path = "secret/ACCESSKEY"
+}
+
+data "vault_generic_secret" "target_node" {
+  path = "secret/NODENAME"
+}
+
+###############################################################################
+# Terraform Plan for lab_two Edge Server instance
+###############################################################################
+
+resource "proxmox_vm_qemu" "lab_two_edge_server" {
+  count = var.numberofvms
+  name  = "${var.yourinitials}-vm${count.index}.service.consul"
+  desc  = var.desc
+  #target_node = data.vault_generic_secret.target_node.data[random_shuffle.nodename.result[0]]
+  target_node = random_shuffle.nodename.result[0]
+  clone       = var.template_to_clone
+  os_type     = "cloud-init"
+  memory      = var.memory
+  cpu {
+    cores   = var.cores
+    sockets = var.sockets
+  }
+  skip_ipv6 = true
+  scsihw    = "virtio-scsi-pci"
+  boot      = "order=virtio0"
+  agent     = 1
+  tags      = var.tags
+
+  ipconfig0 = "ip=dhcp"
+  ipconfig1 = "ip=dhcp"
+  ipconfig2 = "ip=dhcp"
+  #ipconfig3 = "ip=dhcp"
+
+  network {
+    id     = 0
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  network {
+    id     = 1
+    model  = "virtio"
+    bridge = "vmbr1"
+  }
+
+  network {
+    id     = 2
+    #tag    = random_shuffle.vlan.result[0]
+    model  = "virtio"
+    bridge = "vmbr2"
+  }
+
+  #network {
+  #  id     = 3
+  #  #tag    = random_shuffle.vlan.result[0]
+  #  model  = "virtio"
+    # This is the SDN Zone to isolate the launched VM
+  #  bridge = "crvlan"
+  #}
+
+  disks {
+    virtio {
+      virtio0 {
+        disk {
+          iothread = true
+          storage  = random_shuffle.datadisk.result[0]
+          size     = var.disk_size
+        }
+      }
+    }
+  }
+
+  provisioner "remote-exec" {
+    # This inline provisioner is needed to accomplish the final fit and finish of your deployed
+    # instance and configure the system to register the FQDN with the Consul DNS system
+    inline = [
+      "sudo hostnamectl set-hostname ${var.yourinitials}-vm${count.index}",
+      "sudo sed -i 's/changeme/${random_id.id.dec}${count.index}/' /etc/consul.d/system.hcl",
+      "sudo sed -i 's/replace-name/${var.yourinitials}-vm${count.index}/' /etc/consul.d/system.hcl",
+      "sudo sed -i 's/ubuntu-server/${var.yourinitials}-vm${count.index}/' /etc/hosts",
+      "sudo sed -i 's/FQDN/${var.yourinitials}-vm${count.index}.service.consul/' /etc/update-motd.d/999-consul-dns-message",
+      "sudo sed -i 's/#datacenter = \"my-dc-1\"/datacenter = \"rice-dc-1\"/' /etc/consul.d/consul.hcl",
+      "echo 'retry_join = [\"${var.consulip-240-prod-system28}\",\"${var.consulip-240-student-system41}\",\"${var.consulip-242-room}\"]' | sudo tee -a /etc/consul.d/consul.hcl",
+      "sudo systemctl daemon-reload",
+      "sudo systemctl restart consul.service",
+      "sudo rm /opt/consul/node-id",
+      "sudo systemctl restart consul.service",
+      "sudo sed -i 's/0.0.0.0/${var.yourinitials}-vm${count.index}.service.consul/' /etc/systemd/system/node-exporter.service",
+      "sudo systemctl daemon-reload",
+      "sudo systemctl enable node-exporter.service",
+      "sudo systemctl start node-exporter.service",
+      "sudo growpart /dev/vda 3",
+      "sudo lvextend -l +100%FREE /dev/ubuntu-vg/ubuntu-lv",
+      "sudo resize2fs /dev/ubuntu-vg/ubuntu-lv",
+      "echo 'Your FQDN is: ' ; dig +answer -x ${self.default_ipv4_address} +short"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "vagrant"
+      private_key = file("${path.module}/${var.keypath}")
+      host        = self.ssh_host
+      port        = self.ssh_port
+    }
+  }
+}
+
+###############################################################################
+# Terraform Plan for lab_two Node instance
+###############################################################################
+
+resource "proxmox_vm_qemu" "lab_two_node" {
+  count = var.numberofvms
+  name  = "${var.ln_yourinitials}-vm${count.index}.service.consul"
+  desc  = var.desc
+  #target_node = data.vault_generic_secret.target_node.data[random_shuffle.nodename.result[0]]
+  target_node = random_shuffle.nodename.result[0]
+  clone       = var.ln_template_to_clone
+  os_type     = "cloud-init"
+  memory      = var.memory
+  cpu {
+    cores   = var.cores
+    sockets = var.sockets
+  }
+  skip_ipv6 = true
+  scsihw    = "virtio-scsi-pci"
+  boot      = "order=virtio0"
+  agent     = 1
+  tags      = var.ln_tags
+
+  ipconfig0 = "ip=dhcp"
+  ipconfig1 = "ip=dhcp"
+  ipconfig2 = "ip=dhcp"
+  #ipconfig3 = "ip=dhcp"
+
+  network {
+    id     = 0
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  network {
+    id     = 1
+    model  = "virtio"
+    bridge = "vmbr1"
+  }
+
+  network {
+    id     = 2
+    #tag    = random_shuffle.vlan.result[0]
+    model  = "virtio"
+    bridge = "vmbr2"
+  }
+
+  #network {
+  #  id     = 3
+  #  #tag    = random_shuffle.vlan.result[0]
+  #  model  = "virtio"
+    # This is the SDN Zone to isolate the launched VM
+  #  bridge = "crvlan"
+  #}
+
+  disks {
+    virtio {
+      virtio0 {
+        disk {
+          iothread = true
+          storage  = random_shuffle.datadisk.result[0]
+          size     = var.disk_size
+        }
+      }
+    }
+  }
+
+  provisioner "remote-exec" {
+    # This inline provisioner is needed to accomplish the final fit and finish of your deployed
+    # instance and configure the system to register the FQDN with the Consul DNS system
+    inline = [
+      "sudo hostnamectl set-hostname ${var.ln_yourinitials}-vm${count.index}",
+      "sudo sed -i 's/changeme/${random_id.id.dec}${count.index}/' /etc/consul.d/system.hcl",
+      "sudo sed -i 's/replace-name/${var.ln_yourinitials}-vm${count.index}/' /etc/consul.d/system.hcl",
+      "sudo sed -i 's/ubuntu-server/${var.ln_yourinitials}-vm${count.index}/' /etc/hosts",
+      "sudo sed -i 's/FQDN/${var.ln_yourinitials}-vm${count.index}.service.consul/' /etc/update-motd.d/999-consul-dns-message",
+      "sudo sed -i 's/#datacenter = \"my-dc-1\"/datacenter = \"rice-dc-1\"/' /etc/consul.d/consul.hcl",
+      "echo 'retry_join = [\"${var.consulip-240-prod-system28}\",\"${var.consulip-240-student-system41}\",\"${var.consulip-242-room}\"]' | sudo tee -a /etc/consul.d/consul.hcl",
+      "sudo systemctl daemon-reload",
+      "sudo systemctl restart consul.service",
+      "sudo rm /opt/consul/node-id",
+      "sudo systemctl restart consul.service",
+      "sudo sed -i 's/0.0.0.0/${var.ln_yourinitials}-vm${count.index}.service.consul/' /etc/systemd/system/node-exporter.service",
+      "sudo systemctl daemon-reload",
+      "sudo systemctl enable node-exporter.service",
+      "sudo systemctl start node-exporter.service",
+      "sudo growpart /dev/vda 3",
+      "sudo lvextend -l +100%FREE /dev/ubuntu-vg/ubuntu-lv",
+      "sudo resize2fs /dev/ubuntu-vg/ubuntu-lv",
+      "echo 'Your FQDN is: ' ; dig +answer -x ${self.default_ipv4_address} +short"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "vagrant"
+      private_key = file("${path.module}/${var.keypath}")
+      host        = self.ssh_host
+      port        = self.ssh_port
+    }
+  }
+}
--- code/python-api-server/app.py
+++ code/python-api-server/app.py
@@ -199,133 +199,66 @@ def stream(task_id):
     return Response(event_stream(), mimetype="text/event-stream")
 
 ##############################################################################
-# This route capture the request from the CR Dashboard
-##############################################################################
-@app.route('/launch', methods=['POST'])
-def run_launch_command():
-    data = request.get_json()
-    session['runtime_uuid'] = data.get('runtime_uuid')
-    email = data.get('email')
-    username = email.split('@')[0] # the tags do not accept special characters, so we have to split the email address
-    lab_number = data.get('lab_number')
-    src = "/home/cr/CyberRange/build/terraform/proxmox-jammy-ubuntu-cr-lab-templates/" + lab_number
-    dest = "/tmp/" + session['runtime_uuid'] + "/"
-    dest_after_copy = "/tmp/" + session['runtime_uuid'] + "/" + lab_number
-    t = session['runtime_uuid'] + ";" + username + ";" + lab_number + ";" + "cr"
-    logger.info("Data from received HTTP post...", extra={
-    'USER': 'cr',
-    'STATUS': 'success',
-    'VALUE': data
-    })
-
-    # Command to copy the original Terraform plan to a tmp location
-    # (need to store this in session) so it can be retrieved later...
-    # Navigate to the Terraform directory and apply
-    try:
-        logger.info("About to run: mkdir -p " + dest + " ::the mkdir command to create a new directory for the terraform plan files...")
-        result_mkdir = conn.run("mkdir -p " + dest, hide=True)
-        if result_mkdir.exited == 0:
-          logger.info("mkdir -p " + dest + " executed successfully (return 0)")
-        else:
-          logger.info(f"mkdir -p " + dest + " failed with a return code of: {result_mkdir.exited}")
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-    try:
-        logger.info("About to run: cp -r " + src + " "  + dest)
-        result_cp = conn.run("cp" + " -r " + src + " "  + dest, hide=True)
-        if result_cp.exited == 0:
-          logger.info("cp -r " + src + " "  + dest + " executed successfully (return 0)")
-        else:
-          logger.info(f"cp -r " + src + " "  + dest + " failed with a return code of: {result_cp.exited}")
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-    try:
-        logger.info("What directory are we running tf init? " + dest_after_copy)
-        result_tf_init = conn.run("cd " + dest_after_copy + " && " + "terraform init", hide=True) # need to append dest lab_one
-        if result_tf_init.exited == 0:
-            logger.info("cd " + dest_after_copy + " && " + "terraform init executed successfully (return 0)")
-        else:
-            logger.info(f"cd {dest_after_copy} && terraform init failed with a return code of: {result_tf_init.exited}")
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-    try:
-        # Gather all of the runtime terraform vars we will be assigning at terraform apply time
-        vars = {
-            "tags": t,
-            "yourinitials": session['runtime_uuid']
-        }
-
-        # Build the base command
-        cmd = ["terraform", "apply", "-auto-approve"]
-
-        # Append each -var argument
-        for key, value in vars.items():
-            cmd.append(f"-var '{key}={value}'") #syntax -var 'key=value'
-
-        tf_cmd_str = " ".join(cmd)
-        logger.info(f"Constructing the Terraform command...")
-        logger.info(f"About to run: cd {dest_after_copy} ; VAULT_ADDR={vault_addr_build_server} VAULT_TOKEN={vault_token_build_server} VAULT_SKIP_VERIFY={vault_skip_verify_build_server} {tf_cmd_str}")
-        result_cd_tfapply = conn.run(f"cd {dest_after_copy} ; VAULT_ADDR={vault_addr_build_server} VAULT_TOKEN={vault_token_build_server} VAULT_SKIP_VERIFY={vault_skip_verify_build_server} {tf_cmd_str}", pty=True, hide=False)
-        if result_cd_tfapply.exited == 0:
-            logger.info(f"cd {dest_after_copy} ; VAULT_ADDR={vault_addr_build_server} VAULT_TOKEN={vault_token_build_server} VAULT_SKIP_VERIFY={vault_skip_verify_build_server} {tf_cmd_str}" + " executed successfully (return 0)")
-        else:
-            logger.info(f"cd {dest_after_copy} ; VAULT_ADDR={vault_addr_build_server} VAULT_TOKEN={vault_token_build_server} VAULT_SKIP_VERIFY={vault_skip_verify_build_server} {tf_cmd_str} failed with a return code of: {result_cd_tfapply.exited}")
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-    # If everything executes successfully return 1
-    return 1
-##############################################################################
-# This route will launch or destroy the infrastructure for the declared lab
-# via terraform.
-# We need to pass some runtime variables so we can retrieve the metadata
-# later -- this will be done via Python3 Fabric/Invoke/Paramiko
+# This route is more generic and will capture the commands and return one
+# at a time -- making things easier to debug
 ##############################################################################
 @app.route('/destroy', methods=['POST'])
-def run_destroy_command():
+def prepare_destroy_command():
     data = request.get_json()
-    command = data.get('command')
+    uid = data.get('runtime_uuid')
+    email = data.get('email')
+    lab_number = data.get('lab_number')
+    username = email.split('@')[0] # the tags do not accept special characters, so we have to split out the '@' in the email address
 
-    if not command:
-        return jsonify({'error': 'No command provided'}), 400
+    dest_after_copy = "/tmp/" + uid + "/" + lab_number
+    t = uid + ";" + username + ";" + lab_number + ";" + "cr" + ";" + "edge"
+    ln = uid + ";" + username + ";" + lab_number + ";" + "cr"
+    # Gather all of the runtime terraform vars we will be assigning at terraform apply time
+    vars = {
+        "tags": t,
+        "yourinitials": uid,
+        "ln_yourinitials": uid,
+        "ln_tags": ln
+    }
+    # Build the base command
+    cmd = ["terraform", "destroy", "-auto-approve"]
 
-    try:
-        result = subprocess.run(command, shell=True, capture_output=True, text=True)
-        return jsonify({
-            'stdout': result.stdout,
-            'stderr': result.stderr,
-            'returncode': result.returncode
-        })
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    # Append each -var argument
+    for key, value in vars.items():
+        cmd.append(f"-var '{key}={value}'") #syntax -var 'key=value'
 
-##############################################################################
-# This route will use the Proxmoxer API wrapper to authenticate with our
-# Proxmox cluster and query and return the IP of the running edge_node for the
-# SSH function
-##############################################################################
-@app.route('/getip', methods=['POST'])
-def getip():
-    data = request.get_json()
-    command = data.get('command')
+    tf_cmd_str = " ".join(cmd)
+    # Create strings of commands to send to the Celery worker tasks
+    cmd_mkdir="mkdir -p " + dest
+    cmd_cp="cp" + " -r " + src + " "  + dest
+    cmd_tf_init="cd " + dest_after_copy + " && " + " terraform init"
+    cmd_tf_apply = "cd " + dest_after_copy + "; VAULT_ADDR=" + vault_addr_build_server + " VAULT_TOKEN=" + vault_token_build_server +" VAULT_SKIP_VERIFY=" + vault_skip_verify_build_server + " " + tf_cmd_str
+    list_of_commands = [cmd_mkdir,cmd_cp,cmd_tf_init,cmd_tf_apply]
+    
+    # Pass the constructed command to the Celery Task
+    task = run_fabric_command.delay(list_of_commands)
+    return jsonify({"task_id": task.id}), 202
 
-    if not command:
-        return jsonify({'error': 'No command provided'}), 400
 
-    try:
-        result = subprocess.run(command, shell=True, capture_output=True, text=True)
-        return jsonify({
-            'stdout': result.stdout,
-            'stderr': result.stderr,
-            'returncode': result.returncode
-        })
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+@app.route("/status/<task_id>")
+def status(task_id):
+    progress = get_task_progress(task_id)
+    return jsonify(progress)
+
+@app.route("/stream/<task_id>")
+def stream(task_id):
+    def event_stream():
+        last_sent = 0
+        while True:
+            progress = get_task_progress(task_id)
+            if float(progress["timestamp"]) > last_sent:
+                yield f"data: {progress}\n\n"
+                last_sent = float(progress["timestamp"])
+            if progress["status"] in ("SUCCESS", "FAILURE"):
+                break
+            time.sleep(1)
+    return Response(event_stream(), mimetype="text/event-stream")
+
 ##############################################################################
 # Route to handle 404 errors
 # https://copilot.microsoft.com/shares/sA2qT1ngFCwKdEwaExa4R
