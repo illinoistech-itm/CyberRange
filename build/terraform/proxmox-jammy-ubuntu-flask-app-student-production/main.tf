2d204d9b3b3618f74cb8c2c4a79deb3de594790d
Thein Han <than10@hawk.illinoistech.edu>
2026-03-10 22:54:05 -0500

    change line 26 to NODE1 and line 42 to mies-dc-1 in provider.tf

--- build/terraform/proxmox-jammy-ubuntu-flask-app-student-production/main.tf
+++ build/terraform/proxmox-jammy-ubuntu-flask-app-student-production/main.tf
@@ -34,11 +34,11 @@ data "vault_generic_secret" "pm_api_url" {
 }
 
 data "vault_generic_secret" "pm_api_token_id" {
-  path = "secret/SECRETKEY"
+  path = "secret/ACCESSKEY"
 }
 
 data "vault_generic_secret" "pm_api_token_secret" {
-  path = "secret/ACCESSKEY"
+  path = "secret/SECRETKEY"
 }
 
 data "vault_generic_secret" "target_node" {
@@ -124,7 +124,7 @@ resource "proxmox_vm_qemu" "load-balancer" {
       "sudo sed -i 's/replace-name/${var.lb-yourinitials}-vm${count.index}/' /etc/consul.d/system.hcl",
       "sudo sed -i 's/ubuntu-server/${var.lb-yourinitials}-vm${count.index}/' /etc/hosts",
       "sudo sed -i 's/FQDN/${var.lb-yourinitials}-vm${count.index}.service.consul/' /etc/update-motd.d/999-consul-dns-message",
-      "sudo sed -i 's/#datacenter = \"my-dc-1\"/datacenter = \"rice-dc-1\"/' /etc/consul.d/consul.hcl",
+      "sudo sed -i 's/#datacenter = \"my-dc-1\"/datacenter = \"mies-dc-1\"/' /etc/consul.d/consul.hcl",
       "echo 'retry_join = [\"${var.consulip-240-prod-system28}\",\"${var.consulip-240-student-system41}\",\"${var.consulip-242-room}\"]' | sudo tee -a /etc/consul.d/consul.hcl",
       "sudo systemctl daemon-reload",
       "sudo systemctl restart consul.service",
